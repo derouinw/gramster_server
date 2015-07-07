257ae9e09bb1d3b4ae643b3ab65e72944a210905
Bill Derouin <derouinbill@gmail.com>
2015-07-07 04:54:38 +0000

    Update global

--- global.js
+++ global.js
@@ -3,7 +3,7 @@
 // Bill - 6/30/2015
 module.exports = {
   'HOST' : 'http://192.168.1.54',
-  'HOST_LOCAL' : 'http://127.0.0.1',
+  'HOST_LOCAL' : 'http://10.121.14.27',
   'PORT' : '3000',
   'API_IMAGE_VIEW' : '/api/image/view/',
   'API_IMAGE_RECENT' : '/api/image/recent/',
