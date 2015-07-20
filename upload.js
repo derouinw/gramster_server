e4c189ec76407b7d5efd6a427e25713b00c0498a
Bill Derouin <derouinbill@gmail.com>
2015-07-19 19:36:19 -0700

    Update to newest id

--- upload.js
+++ upload.js
@@ -10,7 +10,9 @@ var mime = require('mime');
 var uid = require('uid2');
 var request = require('request');
 var busboy = require('connect-busboy');
+var cassandra = require('cassandra-driver');
 var router = express.Router();
+var client = new cassandra.Client({ contactPoints: ['127.0.0.1'], keyspace: global.KEYSPACE})
 
 var TARGET_PATH = __dirname + '/images/';
 var IMAGE_TYPES = ['image/jpeg', 'image/png'];
@@ -53,25 +55,45 @@ router.post('/', function(req, res) {
 		fstream = fs.createWriteStream(targetPath);
 		file.pipe(fstream);
 		fstream.on('close', function() {
-			var image_data = {
-				id: 2,
-				title: title,
-				path: targetName,
-				description : description
-			};
+			var query = "SELECT value FROM last_id WHERE id_name='posts';";
 
-			var url = global.HOST_LOCAL + ':' + global.PORT + global.API_IMAGE_NEW;
-
-			request.post( {url : url, body : JSON.stringify(image_data)}, function(err, response, body) {
+			client.execute(query, function(err, result) {
 				if (err) {
-					console.log('Upload failed: ' + err);
+					console.log('Error getting new id');
+					return res.status(500).send('Something went wrong?');
 				}
 
-				console.log('Upload successful:' + body);
-				res.writeHead(302, { 
-					'Location' : global.HOST_LOCAL + ':' + global.PORT + global.SITE_IMAGE_VIEW + image_data.id });
-				res.end();
-			});
+				var image_id = result.rows[0].value+1;
+
+				var image_data = {
+					id: image_id,
+					title: title,
+					path: targetName,
+					description : description
+				};
+
+				var url = global.HOST_LOCAL + ':' + global.PORT + global.API_IMAGE_NEW;
+
+				request.post( {url : url, body : JSON.stringify(image_data)}, function(err, response, body) {
+					if (err) {
+						console.log('Upload failed: ' + err);
+						return res.status(500).send('Upload went wrong');
+					}
+
+					console.log('Upload successful:' + body);
+					res.writeHead(302, { 
+						'Location' : global.HOST_LOCAL + ':' + global.PORT + global.SITE_IMAGE_VIEW + image_data.id });
+					res.end();
+				});
+
+				var update_query = "UPDATE last_id SET value=? WHERE id_name='posts'";
+
+				client.execute(update_query, [image_id], {prepare: true}, function(err, result) {
+					if (err) {
+						console.log('Update id went wrong');
+					}
+				});
+			});			
 		});
 	});
 });
